b39a52bcea89df8268170ced08d63450b9f9bb5c
Nikita <nikitos54345@gmail.com>
2025-03-20 10:39:19 +0300

    fix

--- src/components/PopUpHint.tsx
+++ src/components/PopUpHint.tsx
@@ -14,7 +14,7 @@ const PopUpHint = () => {
       animate={{ opacity: 1, transition: {duration: 1, ease: 'easeInOut'}}}
       exit={{opacity: 0, transition: {duration: .5, ease: 'easeInOut'}}}
     >
-        <motion.div className='bg-black opacity-50 h-[85%] w-full absolute bottom-0'>
+        <motion.div className='bg-black opacity-80 h-[85%] w-full absolute bottom-0'>
         </motion.div>
         <motion.div className='p-4 py-6 bg-gray-700 rounded-xl text-gray-300 inline-block absolute top-35 left-70 '>Drag this pannel down to change the active page</motion.div>
     </motion.div>
